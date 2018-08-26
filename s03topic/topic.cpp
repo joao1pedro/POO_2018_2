eb76e99bb6e941efd35b52d69f45379427698f5b
Joao <jotape.ufc@gmail.com>
2018-08-25 21:15:43 -0300

    atualização da pratica

--- s03topic/topic.cpp
+++ s03topic/topic.cpp
@@ -9,9 +9,8 @@ struct Passageiro{
 
     Passageiro(string id = "fulano", string idade = "0"){
         this->id = id;
-        this ->idade = idade;
+        this->idade = idade;
     }
-
     string toString(){
         stringstream ss;
         ss << this->id << ":" << this->idade;
@@ -20,10 +19,10 @@ struct Passageiro{
 };
 
 struct Topic{
-    vector <Passageiro*> cadeiras;
+    vector<Passageiro*> cadeiras;
 
-    Topic(int capMax = 0, int qtdPref = 0):
-        cadeiras(capMax, nullptr)
+    Topic(int qtd = 0):
+        cadeiras(qtd, nullptr)
     {
     }
 
@@ -32,10 +31,34 @@ struct Topic{
             delete(pass);
     }
 
+    bool inPass(Passageiro * passageiro) {
+        for(int i = 0; i < (int) cadeiras.size(); i++){
+            if(cadeiras[i] == nullptr){
+                cadeiras[i] = passageiro;
+                return true;
+            }
+            else if(cadeiras[i]->id == passageiro->id){
+                cout << "fail: o passageiro ja esta na topic" << endl;
+                return false;
+            }
+        }
+        return false;
+    }
+
+    bool outPass(string idPass){
+        for(int i =0; i< (int) cadeiras.size(); i++){
+            if(idPass == cadeiras[i]->id){
+                cadeiras[i] = nullptr;
+                return true;
+            }
+        }
+        return false;
+    }
+
     string toString(){
         stringstream ss;
         ss << "[ ";
-        for(Passageiro* passageiro : cadeiras){
+        for(Passageiro * passageiro : cadeiras){
             if(passageiro != nullptr)
                 ss << passageiro->toString() << " ";
             ss << "-" << "  ";
@@ -45,22 +68,31 @@ struct Topic{
     }
 };
 
-int main()
-{
+int main(){
     Topic topic;
     string op;
-
     while(true){
         cin >> op;
-        if(op == "show")
+        if(op == "show"){
             cout << topic.toString() << endl;
+        }
         else if(op == "new"){
-            int capMax, qtdPref;
-            cin >> capMax >> qtdPref;
-            topic = Topic(capMax, qtdPref);
+            int qtd;
+            cin >> qtd;
+            topic = Topic(qtd);
+            cout << "Topic criada com " << qtd << " cadeiras" << endl;
+        }
+        else if(op == "in"){
+            string id, idade;
+            cin >> id >> idade;
+            if(topic.inPass(new Passageiro(id, idade)))
+                cout << "done" << endl;
+        }
+        else if(op == "out"){
+            string idPass;
+            cin >> idPass;
+            if(topic.outPass(idPass))
+                cout << "done" << endl;
         }
     }
-    
-    return 0;
 }
-
