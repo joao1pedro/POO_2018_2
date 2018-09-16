4b712a2452e6c14e7da58b3dccb849a12b374697
Joao <jotape.ufc@gmail.com>
2018-09-16 16:26:21 -0300

    update agiota

--- agiota/agiota.cpp
+++ agiota/agiota.cpp
@@ -3,24 +3,20 @@
 #include <sstream>
 using namespace std;
 
-class Cliente{
-private:
+struct Cliente{
     string keyID;
     string id;
-    int saldo;
+    float saldo;
     bool alive{true};
-
-public: 
-    Cliente(string keyID = "", string id = "", int saldo = 0):
+ 
+    Cliente(string keyID = "", string id = "", float saldo = 0):
         keyID(keyID), id(id), saldo(saldo)
     {
     }
 
     string toString(){
         stringstream ss;
-
         ss << this->keyID << ":" << this->id << ":" << this->saldo;
-
         return ss.str();
     }
 
@@ -33,13 +29,115 @@ public:
     
 };
 
-struct Agiota{
-    int saldoAmigo;
-    vector<Cliente> clientes;
+struct Movimentacao{
+    string key;
+    float value;
+    Movimentacao(string key = "", float value = 0):
+        key(key), value(value)
+    {
+    }
+};
 
-    Agiota(int saldoAmigo = 0):
+struct Agiota{
+    float saldoAmigo;
+    vector<Cliente> clientes;
+    vector<Movimentacao> mov;
+
+    Agiota(float saldoAmigo = 0):
         saldoAmigo(saldoAmigo)
     {
+    }
+
+    string toString(){
+        stringstream ss;
+        for(int i = 0; i < (int) clientes.size(); i++){
+            ss << clientes[i].toString() << endl;
+        }
+        return ss.str();
+    }
+
+    string saldoAmigoT(){
+        stringstream ss;
+        ss << "Saldo amigo: " << this->saldoAmigo;
+        return ss.str();
+    }
+
+    bool add(string key, string id){
+        for(int i = 0; i < (int) clientes.size(); i++){
+            if(clientes[i].keyID == key){
+                cout << "fail: cliente " << key << " ja existe" << endl;
+                return false;
+            }
+        }
+        clientes.push_back(Cliente(key, id, 0));
+        return true;
+    }
+
+    bool emprestimo(string key, float value){
+        if(value > saldoAmigo){
+            cout << "fail: saldo do agiota eh insuficiente" << endl;
+            return false;
+        }
+        for(int i = 0; i < (int) clientes.size(); i++){
+            if(clientes[i].keyID == key){
+                clientes[i].saldo = clientes[i].saldo+value;
+                saldoAmigo = saldoAmigo-value;
+                mov.push_back(Movimentacao(key, value));
+                return true;
+            }
+        }
+        cout << "fail: cliente nao existe" << endl;
+        return false;
+    }
+
+     string toString_transferencias(){
+        stringstream ss;
+        for(int i = 0; i < (int) mov.size(); i++)
+            ss << "id: "<< i <<"[ "<< mov[i].key << " "<< mov[i].value <<" ]";
+        return ss.str();
+    }
+
+    string indiv_toString(string key){
+        stringstream ss;
+        for(int i = 0; i < (int) mov.size(); i++){
+            if( key == mov[i].key)
+                ss << "id:"<< i << " [ "<<mov[i].key << " " << mov[i].value <<" ]";
+        }
+        return ss.str();
+    }
+    bool kill(string key){
+        for(int i = 0; i < (int) clientes.size(); i++){
+            if(clientes[i].keyID == key){
+                clientes.erase(clientes.begin() + i);
+                for(int j = 0; j < (int) mov.size(); j++){
+                    if(mov[j].key == key)
+                        mov.erase(mov.begin() + j);
+                }
+                cout << "success: " << key << " foi morto";
+                clientes[i].alive = false;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool pay(string key, float value){
+        for(int i = 0; i < (int) clientes.size(); i++){
+            if(clientes[i].keyID == key){
+                if(value > clientes[i].saldo){
+                    cout << "fail: pagamento maior que a divida nao eh aceitavel" << endl;
+                    return false;
+                }
+                else{
+                    clientes[i].saldo = clientes[i].saldo+value;
+                    mov.push_back(Movimentacao(key, value));
+                    saldoAmigo = saldoAmigo+value;
+                    return true;
+                }
+            }
+        }
+        cout << "fail: devedor nao existe" << endl;
+        return false;
     }
 
 };
@@ -55,21 +153,59 @@ struct Controller{
         ui >> op;
 
         if(op == "help"){
-            out << "show cli; add _cliChave _nomeCliente; add tr _value; kill _cliChave; clear";
+            out << "showCli; showTr; show _cliChave; addCli _cliChave _nomeCliente; addTr _cliChave _value; kill _cliChave; consult;clear;";
         }
-        else if(op == "showcli"){
-            out << cliente.toString();
+        else if(op == "showCli"){
+            out << a.toString();
         }
         else if(op == "init"){
-            int value;
+            float value;
             ui >> value;
             a = Agiota(value);
-            out << "credito Agiota criado com " << value;
+            out << "success";
         }
-        else if(op.compare("add cli")){
-            //out << 55;
+        else if(op == "addCli"){
+            string id, key;
+            ui >> key;
+            getline(ui, id);
+            if(a.add(key, id))
+                out << "success";
         }
-
+        else if(op == "addTr"){
+            string key;
+            float value;
+            ui >> key >> value;
+            if(a.emprestimo(key, value))
+                out << "success";
+        }
+        else if(op == "showTr"){
+            string key;
+            ui >> key;
+            out << a.toString_transferencias();
+        }
+        else if(op == "show"){
+            string key;
+            ui >> key;
+            out << a.indiv_toString(key);
+        }
+        else if(op == "kill"){
+            string key;
+            ui >> key;
+            a.kill(key);
+        }
+        else if(op == "consult"){
+            out << a.saldoAmigoT();
+        }
+        else if(op == "pay"){
+            string key;
+            float value;
+            ui >> key >> value;
+            if(a.pay(key, value))
+                out << "success";
+        }
+        else if(op == "clear"){
+            system("clear");
+        }
         return out.str();
     }
 
@@ -81,6 +217,7 @@ struct Controller{
             getline(cin, line);
             if(line == "end")
                 break;
+            //cout << line << endl;
             cout << "  " << shell(line) << endl;
         }
     }
@@ -89,6 +226,5 @@ struct Controller{
 int main(){
     Controller c;
     c.ui();
-
     return 0;
 }
