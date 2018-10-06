679c2fdd119d50f9bfb560f4c235278e62420dfb
Joao <jotape.ufc@gmail.com>
2018-10-06 12:31:20 -0300

    update

--- Repositorio1/repositorio.cpp
+++ Repositorio1/repositorio.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <map>
 #include <sstream>
+#include <vector>
 using namespace std;
 
 class Professor{
 public:
     string nome;
     string siape;
+public:
     Professor(string nome = "", string siape = ""):
         nome(nome), siape(siape)
         {
         }
+    string toString(){
+        return siape + ":" + nome;
+    }
 };
 
 class Aluno{
@@ -18,10 +23,14 @@ public:
     string matricula;
     string curso;
     string nome;
-
+public:
     Aluno(string matricula = "", string curso = "", string nome = ""):
         matricula(matricula), curso(curso), nome(nome)
     {
+    }
+
+    string toString(){
+        return matricula + ":" + curso + ":" + nome;
     }
 };
 
@@ -29,11 +38,15 @@ class Disciplina{
     string codigo;
     string curso;
     string nome;
-
+public:
     Disciplina(string codigo = "", string curso = "", string nome = ""):
         codigo(codigo), curso(curso), nome(nome)
     {
     } 
+
+    string toString(){
+        return codigo + ":" + nome;
+    }
 };
 
 template<typename T>
@@ -44,24 +57,24 @@ struct Repositorio{
         if(!exists(key))
             data[key] = elem;
         else
-            throw "chave duplicada";
+            throw "fail: chave ja existe";
     }
     
     bool exists(string key){
         return data.find(key) != data.end();
     }
 
-    T& get(string key){
+    string get(string key){
         auto it = data.find(key);
         if(it != data.end())
-            return it->second;
+            return it->second.toString();
         else
-            throw "chave nao existe";
+            throw "fail: chave nao existe";
     }
 
     void rm(string key){
         if(!exists(key))
-            throw "chave nao existe";
+            throw "fail: chave nao existe";
         data.erase(key);
     }
 
@@ -78,8 +91,125 @@ struct Repositorio{
             vp.push_back(par.first);
         return vp;
     }
+
+    string showAll(){
+        stringstream ss;
+        for(auto it : data){
+            ss << "  " <<  it.second.toString() << endl;
+        }
+        return ss.str();
+    }
 };
 
 class Controller{
+public:
+    Repositorio <Aluno> alunos;
+    Repositorio <Disciplina> disciplinas;
+    Repositorio <Professor> professores;
 
+    string shell(string line){
+        stringstream ui(line);
+        stringstream out;
+        string op;
+        ui >> op;
+        try{
+            if(op == "help"){
+                out << "addAlu _matriccula _curso _nome"
+                    << " addPro _siape _nome"
+                    << " addDis _codigo _nome"
+                    << " getAlu _matricula"
+                    << " getPro _siape"
+                    << " getDis _codigo"
+                    << " showAlu"
+                    << " showPro"
+                    << " showDisc"
+                    << " rmAlu _matricula"
+                    << " rmPro _siape"
+                    << " rmDis _codigo"
+                    << " clear"
+                    << " end" << endl;
+            }else if(op == "addAlu"){
+                string matricula, curso, nome;
+                ui >> matricula >> curso;
+                getline(ui, nome);
+                alunos.add(matricula, Aluno(matricula, curso, nome));
+            }
+            else if(op == "addDis"){
+                string codigo, nome;
+                ui >> codigo;
+                getline(ui, nome);
+                disciplinas.add(codigo, Disciplina(codigo, nome));
+            }
+            else if(op == "addPro"){
+                string siape, nome;
+                ui >> siape;
+                getline(ui, nome);
+                professores.add(siape, Professor(nome, siape));
+            }
+            else if(op == "showAlu"){
+                out << alunos.showAll();
+            }
+            else if(op == "showPro"){
+                out << professores.showAll();
+            }
+            else if(op == "showDisc"){
+                out << disciplinas.showAll();
+            }
+            else if(op == "getAlu"){
+                string key;
+                ui >> key;
+                out << alunos.get(key);
+            }
+            else if(op == "getDis"){
+                string key;
+                ui >> key;
+                out << disciplinas.get(key);
+            }
+            else if(op == "getPro"){
+                string key;
+                ui >> key;
+                out << professores.get(key);
+            }
+            else if(op == "rmAlu"){
+                string key;
+                ui >> key;
+                alunos.rm(key);
+            }
+            else if(op == "rmDis"){
+                string key;
+                ui >> key;
+                disciplinas.rm(key);
+            }
+            else if(op == "rmPro"){
+                string key;
+                ui >> key;
+                professores.rm(key);
+            }
+        }
+        catch(const char* e){
+            cout << e << endl;
+        }
+        return out.str();
+    }
+
+    void ui(){
+        string line;
+        stringstream ss;
+    
+        while(true){
+            getline(cin, line);
+            if(line == "end")
+                break;
+            //cout << line << endl;
+            cout << "  " << shell(line) << endl;
+        }
+    }
 };
+
+int main()
+{
+    Controller c;
+    c.ui();
+    return 0;
+}
+
